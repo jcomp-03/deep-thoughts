28d00521b6be6ae13d0460838afa9a84ba9f70de
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-28 22:15:06 -0400

    Developed client-side login and sign-up functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
@@ -20,9 +20,18 @@ import Signup from './pages/Signup';
 const httpLink = createHttpLink({
   uri: "/graphql",
 });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client';
+
+// we import the gql tagged template literal functionality 
+// to create a GraphQL mutation called login. This accepts two
+// variables $email and $password whose values we'll set up to
+// passed in as arguments when we integrate this with the login form
+// page.    
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
